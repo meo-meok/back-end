1fde5da14df7648323376890e2708d3f6195c5f1
JamieSKinard <hgu21600137@gmail.com>
2022-08-09 15:54:52 +0900

    db model name correction

--- meomeok/models.py
+++ meomeok/models.py
@@ -7,7 +7,7 @@ class Users(models.Model) :
     user_name = models.CharField(max_length = 5)
     authority = models.IntegerField(default = 0)
 
-class Restarunt(models.Model) :
+class Restaurant(models.Model) :
     rest_id = models.IntegerField(default = 1)
     rest_name = models.CharField(max_length = 30)
     rest_number = models.CharField(max_length = 30)
@@ -17,17 +17,17 @@ class Restarunt(models.Model) :
     
 class Review (models.Model) :
     user = models.ForeignKey(Users, on_delete=models.CASCADE)
-    rest = models.ForeignKey(Restarunt, on_delete=models.CASCADE)
-    rest_star = models.FloatField(default = 0.0)
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    star = models.FloatField(default = 0.0)
     post_date = models.DateTimeField('date published')
     post_body = models.TextField()
 
 class menu_price (models.Model) :
-    rest = models.ForeignKey(Restarunt, on_delete = models.CASCADE)
+    restaurant = models.ForeignKey(Restaurant, on_delete = models.CASCADE)
     menu_name = models.CharField(max_length=20)
     menu_price = models.IntegerField()
 
-class rest_location (models.Model) :
-    rest = models.ForeignKey(Restarunt, on_delete = models.CASCADE)
+class location (models.Model) :
+    restaurant = models.ForeignKey(Restaurant, on_delete = models.CASCADE)
     x_axis = models.DecimalField(max_digits=9, decimal_places=6)
     y_axis = models.DecimalField(max_digits=9, decimal_places=6)
